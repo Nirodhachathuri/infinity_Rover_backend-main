df92fb30426138636eb9170686efc272ba693e0b
tharindu_107365 <tharindu.madhushan@axiatadigitallabs.com>
2024-01-14 10:04:05 +0530

    h

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,7 @@
 import {Sequelize} from "sequelize";
 
-const db = new Sequelize('new_test_db','root','password',{
-// const db = new Sequelize('new_test_db','root','',{
-    host: "localhost",
+Sequelize('gamification_123','gamification_123','12345678',{
+    host: "db4free.net",
     dialect: "mysql"
 });
 
