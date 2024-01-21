b8bb91ec3bb5804564a802d8d702827526511f7f
tharindumadhushan17 <tharindumadhushan17@gmail.com>
2024-01-21 13:32:58 +0530

    fixes

--- controllers/Recharge.js
+++ controllers/Recharge.js
@@ -1,11 +1,11 @@
-import TronWeb from 'tronweb';
-import ora from 'ora';
+import TronWeb from "tronweb";
+import ora from "ora";
 import Users from "../models/UserModel.js";
 import Rechge from "../models/RechargeModel.js";
-import History from '../models/HistoryModel.js';
+import History from "../models/HistoryModel.js";
 import { Sequelize } from "sequelize";
 
-const { Op } = Sequelize
+const { Op } = Sequelize;
 
 // Recharges -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -13,98 +13,105 @@ const { Op } = Sequelize
 // Hash: 94f7eeea182b28a7e62f322a7c6bb71ad50e7ee3222229849731274594d09903
 
 export const Recharge = async (req, res) => {
- 
-   
-   const load = ora({
-     color: 'green',
-     hideCursor: true
-   }).start();
-   const { username, balance, userId, hash, amount } = req.body;
-   const txid = hash.length;
-   const status = "Success";
-   const minimun = 6;
-   const amountAll = req.body.amount * 1000000;
-   const receiver = process.env.RECEIVER;
-   const network = "mainnet";
-   let url = null 
-    if (network === "shasta") {
-      url = "";
-    } else if (network === "nile") {
-      url = "";
-    } else if(network === "mainnet") {
-      url = "";
-    }
-    
-    const can = await ( minimun <= amount );
-      if(!can) return res.status(400).json({msg: "Very Low Amount"});
-       if(Math.round(amount) != amount) {
-       return res.status(403).json({msg: "Only Number Integer"});
-    }
-    const verifyHash = await ( 64 <= txid );
-      if(!verifyHash) {
-      return res.status(404).json({msg: "The Hash Is Invalid"});
-    }
-    const validateHash = await Rechge.findOne({ where: { hash: hash } });
-      if(validateHash) {
-       return res.status(401).json({msg: "The Hash In System"});
-    } else {
-  
+  const load = ora({
+    color: "green",
+    hideCursor: true,
+  }).start();
+  const { username, balance, userId, hash, amount } = req.body;
+  const txid = hash.length;
+  const status = "Success";
+  const minimun = 6;
+  const amountAll = req.body.amount * 1000000;
+  const receiver = process.env.RECEIVER;
+  const network = "mainnet";
+  let url = null;
+  if (network === "shasta") {
+    url = "";
+  } else if (network === "nile") {
+    url = "";
+  } else if (network === "mainnet") {
+    url = "";
+  }
 
-   try {
-      
-    console.log("Checking Recharge...");   
-       const tronWeb = new TronWeb({
-       fullHost: url
-       });
+  const can = await (minimun <= amount);
+  if (!can) return res.status(400).json({ msg: "Very Low Amount" });
+  if (Math.round(amount) != amount) {
+    return res.status(403).json({ msg: "Only Number Integer" });
+  }
+  const verifyHash = await (64 <= txid);
+  if (!verifyHash) {
+    return res.status(404).json({ msg: "The Hash Is Invalid" });
+  }
+  const validateHash = await Rechge.findOne({ where: { hash: hash } });
+  if (validateHash) {
+    return res.status(401).json({ msg: "The Hash In System" });
+  } else {
+    try {
+      console.log("Checking Recharge...");
+      const tronWeb = new TronWeb({
+        fullHost: url,
+      });
 
-       const verifyTx = await tronWeb.getEventByTransactionID(hash); 
-       const addressBs58 = tronWeb.address.fromHex(verifyTx[0].result.to);
-        
-       if(receiver != addressBs58) {
-        return res.status(403).json({msg:"Transfer Be Invalid"});
-     } if(amountAll != verifyTx[0].result.value) {
-        return res.status(401).json({msg:"Amount Be Invalid"});
-             
+      const verifyTx = await tronWeb.getEventByTransactionID(hash);
+      const addressBs58 = tronWeb.address.fromHex(verifyTx[0].result.to);
+
+      if (receiver != addressBs58) {
+        return res.status(403).json({ msg: "Transfer Be Invalid" });
+      }
+      if (amountAll != verifyTx[0].result.value) {
+        return res.status(401).json({ msg: "Amount Be Invalid" });
       }
 
-  
-    await Rechge.create(
-      {
-         username: username,  
-         hash: hash,
-         amount: amount,
-         status: status
+      await Rechge.create({
+        username: username,
+        hash: hash,
+        amount: amount,
+        status: status,
       });
-    load.succeed("Has been checked!");
+      load.succeed("Has been checked!");
 
-    const updateBalance = await( balance + parseInt(amount) );
-    await Users.update(
-       {
-         balance: updateBalance
-       },
-       {
-        where:{
-         id: userId
-       }
-    });
-    await History.create({
-      userId: userId,
-      amount: amount,
-      balanceIncreased: true,
-      type:"recharge"
-    });
-       load.succeed("Success Operation!");
-       return res.status(200).json({msg: "Success Operation"});
-  
+      const updateBalance = await (balance + parseInt(amount));
+      await Users.update(
+        {
+          balance: updateBalance,
+        },
+        {
+          where: {
+            id: userId,
+          },
+        }
+      );
+      await History.create({
+        userId: userId,
+        amount: amount,
+        balanceIncreased: true,
+        type: "recharge",
+      });
+      load.succeed("Success Operation!");
+      return res.status(200).json({ msg: "Success Operation" });
     } catch (error) {
       console.log(error);
-      res.status(400).json({msg:"Operation Failed"});
-      };      
-   }
-}
+      res.status(400).json({ msg: "Operation Failed" });
+    }
+  }
+};
 
+export const CreateRecharge = async (req, res) => {
+  try {
+    await Recharge.create({
+      username: req.body.username,
+      amount: req.body.amount,
+      status: 'Pending',
+      // Assuming you store the file path in the database
+      // imagePath: req.file.path,
+    });
+    res.status(200).json({ msg: "Register Success" });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ msg: "Operation Failed!" });
+  }
+};
 export const GetTodayDepositsCount = async (req, res) => {
-
   try {
     const today = new Date();
     today.setHours(0, 0, 0, 0); // Set the time to midnight
@@ -117,16 +124,14 @@ export const GetTodayDepositsCount = async (req, res) => {
         },
       },
     });
-    return res.status(200).json({count :count,  msg: "successfull" });
+    return res.status(200).json({ count: count, msg: "successfull" });
   } catch (error) {
     console.log(error);
     res.status(404).json({ msg: "Operation Failed!" });
   }
-  
 };
 
 export const GetTodayDepositDetails = async (req, res) => {
-
   try {
     const today = new Date();
     today.setHours(0, 0, 0, 0); // Set the time to midnight
@@ -138,12 +143,11 @@ export const GetTodayDepositDetails = async (req, res) => {
         },
       },
     });
-    return res.status(200).json({deposits :deposits,  msg: "successfull" });
+    return res.status(200).json({ deposits: deposits, msg: "successfull" });
   } catch (error) {
     console.log(error);
     res.status(404).json({ msg: "Operation Failed!" });
   }
-  
 };
 
 // ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,33 @@
 import express from "express";
-import { upload, Register, Login, Logout, SetPosition, GetIntroducerSet, GetNewCommerSet, GetIrAllowance,updateWallet, ApproveKYCForUser, GetNewRegistrations, GetBannedUsers,GetIrFamily ,GetAllUsers,GetIrAllowance1} from "../controllers/Users.js";
+
+import {
+  upload,
+  Register,
+  Login,
+  Logout,
+  SetPosition,
+  GetIntroducerSet,
+  GetNewCommerSet,
+  GetIrAllowance,
+  updateWallet,
+  ApproveKYCForUser,
+  GetNewRegistrations,
+  GetBannedUsers,
+  GetIrFamily,
+  GetAllUsers,
+  GetIrAllowance1,
+} from "../controllers/Users.js";
 import {
   GetTodayWithdrawalDetails,
   GetTodayWithdrawalsCount,
   GetTotalWithdrawn,
-  Withdraw
+  Withdraw,
 } from "../controllers/Withdraw.js";
-import { GetTodayDepositDetails, GetTodayDepositsCount, Recharge } from "../controllers/Recharge.js";
+import {
+  GetTodayDepositDetails,
+  GetTodayDepositsCount,
+  Recharge,
+} from "../controllers/Recharge.js";
 import {
   WithdrawalsHistory,
   RechargesHistory,
@@ -23,53 +44,63 @@ import {
   AddPackage,
   BuyPackage,
   GetAllPackages,
-  GetPackagesByUser,GetAllByPackages,
+  GetPackagesByUser,
+  GetAllByPackages,
 } from "../controllers/Package.js";
-import { ChangeStatus, GetAdminControls, GetControls, UpdateAdminControls } from "../controllers/ControlPanel.js";
+import {
+  ChangeStatus,
+  GetAdminControls,
+  GetControls,
+  UpdateAdminControls,
+} from "../controllers/ControlPanel.js";
+import {
+  CreateRecharge,
+} from "../controllers/Recharge.js";
 
 const router = express.Router();
 
-router.get('/users', verifyToken);
-router.get('/token', refreshToken);
-router.post('/levelOne', levelOne);
-router.post('/levelTwo', levelTwo);
-router.post('/levelPremium', levelPremium);
-router.post('/address', SetAddress);
-router.post('/register',upload.array("images", 5), Register);
-router.post('/login', Login);
-router.post('/withdraw', Withdraw);
-router.post('/recharge', Recharge);
-router.post('/tasks', Task);
-router.all('/withdraw/history', WithdrawalsHistory);
-router.all('/recharge/history', RechargesHistory);
-router.all('/tasks/history', TasksHistory);
-router.all('/team', Team);
-router.delete('/logout', Logout);
-router.put('/setposition', SetPosition); // should be provide user_id, position, loged_user_id as a body data
-router.get('/introducer/get/:ref_code', GetIntroducerSet); // should be provide ref_code of loged user as on params 
-router.post('/addpackage', AddPackage); // should be provide packageName:string, packageValue:float
-router.post('/buypackage', BuyPackage); // should be provide packageId:int, userId:int
-router.get('/getpackages/:id', GetPackagesByUser);// should be user id as a param data
-router.get('/getallpackages', GetAllPackages);// should be user id as a param data
-router.get('/getcontrols', GetControls);// should be user id as a param data
-router.put('/changeStatus', ChangeStatus);// should be user id as a param data
-router.get('/gethistory/:id', getAllHistory); // should be user id as a param data
-router.get('/newcommer/get/:user_code', GetNewCommerSet); // should be provide ref_code of loged user as on params 
-router.get('/getIrFamily/get/:user_code', GetIrFamily); // should be provide ref_code of loged user as on params 
-router.get('/irallowance/:user_code', GetIrAllowance); // should be provide ref_code of loged user as on params 
-router.put('/updateWallet', updateWallet); // should be provide ref_code of loged user as on params 
-router.put('/kyc/approve', ApproveKYCForUser); // should be provide userId and approved data  as on bodydata
-router.get('/withdraw/todaycount', GetTodayWithdrawalsCount); 
-router.get('/withdraw/todaydetails', GetTodayWithdrawalDetails); 
-router.get('/withdraw/total/:userId', GetTotalWithdrawn);
-router.get('/recharge/todaycount', GetTodayDepositsCount);
-router.get('/recharge/todaydetails', GetTodayDepositDetails); 
-router.get('/users/newregistrations', GetNewRegistrations); 
-router.get('/users/bannedusers', GetBannedUsers); 
-router.get('/systemcontrols', GetAdminControls);
-router.put('/systemcontrols/update', UpdateAdminControls);
-router.get('/GetAllUsers', GetAllUsers);
-router.get('/GetIrAllowance1/:id', GetIrAllowance1);
-router.get('/GetAllByPackages', GetAllByPackages);
+router.get("/users", verifyToken);
+router.get("/token", refreshToken);
+router.post("/levelOne", levelOne);
+router.post("/levelTwo", levelTwo);
+router.post("/levelPremium", levelPremium);
+router.post("/address", SetAddress);
+router.post("/register", upload.array("images", 5), Register);
+router.post("/login", Login);
+router.post("/withdraw", Withdraw);
+router.post("/recharge", Recharge);
+router.post("/tasks", Task);
+router.all("/withdraw/history", WithdrawalsHistory);
+router.all("/recharge/history", RechargesHistory);
+router.all("/tasks/history", TasksHistory);
+router.all("/team", Team);
+router.delete("/logout", Logout);
+router.put("/setposition", SetPosition); // should be provide user_id, position, loged_user_id as a body data
+router.get("/introducer/get/:ref_code", GetIntroducerSet); // should be provide ref_code of loged user as on params
+router.post("/addpackage", AddPackage); // should be provide packageName:string, packageValue:float
+router.post("/buypackage", BuyPackage); // should be provide packageId:int, userId:int
+router.get("/getpackages/:id", GetPackagesByUser); // should be user id as a param data
+router.get("/getallpackages", GetAllPackages); // should be user id as a param data
+router.get("/getcontrols", GetControls); // should be user id as a param data
+router.put("/changeStatus", ChangeStatus); // should be user id as a param data
+router.get("/gethistory/:id", getAllHistory); // should be user id as a param data
+router.get("/newcommer/get/:user_code", GetNewCommerSet); // should be provide ref_code of loged user as on params
+router.get("/getIrFamily/get/:user_code", GetIrFamily); // should be provide ref_code of loged user as on params
+router.get("/irallowance/:user_code", GetIrAllowance); // should be provide ref_code of loged user as on params
+router.put("/updateWallet", updateWallet); // should be provide ref_code of loged user as on params
+router.put("/kyc/approve", ApproveKYCForUser); // should be provide userId and approved data  as on bodydata
+router.get("/withdraw/todaycount", GetTodayWithdrawalsCount);
+router.get("/withdraw/todaydetails", GetTodayWithdrawalDetails);
+router.get("/withdraw/total/:userId", GetTotalWithdrawn);
+router.get("/recharge/todaycount", GetTodayDepositsCount);
+router.get("/recharge/todaydetails", GetTodayDepositDetails);
+router.get("/users/newregistrations", GetNewRegistrations);
+router.get("/users/bannedusers", GetBannedUsers);
+router.get("/systemcontrols", GetAdminControls);
+router.put("/systemcontrols/update", UpdateAdminControls);
+router.get("/GetAllUsers", GetAllUsers);
+router.get("/GetIrAllowance1/:id", GetIrAllowance1);
+router.get("/GetAllByPackages", GetAllByPackages);
+router.post("/CreateRecharge",CreateRecharge);
 
 export default router;
